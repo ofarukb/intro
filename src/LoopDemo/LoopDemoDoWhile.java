224b2e0ccd5bfa3ddd2d94a848f98b5b4ab3659f
omerfarukbala <omerfarukbala@gmail.com>
2024-03-06 15:14:59 +0300

    do-while loop demosu

---
+++ src/LoopDemo/LoopDemoDoWhile.java
@@ -0,0 +1,15 @@
+package LoopDemo;
+
+public class LoopDemoDoWhile {
+
+	public static void main(String[] args) {
+		
+		int i=2;
+		
+		do {
+			System.out.println(i);
+			i+=2;
+		} while (i<10);
+	}
+
+}
