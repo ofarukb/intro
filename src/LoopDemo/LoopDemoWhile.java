35dd4e1cf012929faf6cd888643b25992a4000c0
omerfarukbala <omerfarukbala@gmail.com>
2024-03-06 15:08:51 +0300

    while loop demosu

---
+++ src/LoopDemo/LoopDemoWhile.java
@@ -0,0 +1,14 @@
+package LoopDemo;
+
+public class LoopDemoWhile {
+
+	public static void main(String[] args) {
+		int i = 3;
+		
+		while (i <= 15) {
+			System.out.println(i);
+			i+=3;
+		}
+	}
+
+}
