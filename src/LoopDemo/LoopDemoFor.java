91743b279f6f8de6d2b257a69b7cfe03b02e2d68
omerfarukbala <omerfarukbala@gmail.com>
2024-03-06 15:01:12 +0300

    for loop demosu

---
+++ src/LoopDemo/LoopDemoFor.java
@@ -0,0 +1,13 @@
+package LoopDemo;
+
+
+public class LoopDemoFor {
+	
+	public static void main(String[] args) {
+		for (int i = 0; i < 30; i+=2) {
+			System.out.println(i);
+		}
+		System.out.println("Döngü bitti.");
+	}
+
+}
